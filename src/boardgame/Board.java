d288a11053930a75d3e1c9f59795cc9aacda4443
cedricapollon <gatuno0609@gmail.com>
2020-06-23 01:22:24 +0100

    Placing on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -41,5 +41,10 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
-
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()]=piece;
+		piece.position=position;
+	}
+	
+	
 }
